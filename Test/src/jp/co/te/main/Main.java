923c625821cbf49ff90bb8424a6c95703438d65b
tailedge <tailedge24@gmail.com>
2015-03-28 15:49:03 +0900

    add handle exception logic

--- Test/src/jp/co/te/main/Main.java
+++ Test/src/jp/co/te/main/Main.java
@@ -2,9 +2,13 @@ package jp.co.te.main;
 
 public class Main {
 	public static void main(String[] args) {
-		new Main().execute();
+		try {
+			new Main().execute();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
 	private void execute() {
 		System.out.println("Hello, world.");
 	}
