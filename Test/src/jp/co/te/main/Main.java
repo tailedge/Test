09259f25cde78aac00e300e13bb5d1f53a81ce48
tailedge <tailedge24@gmail.com>
2015-03-28 15:46:23 +0900

    add execute method

--- Test/src/jp/co/te/main/Main.java
+++ Test/src/jp/co/te/main/Main.java
@@ -2,6 +2,10 @@ package jp.co.te.main;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		new Main().execute();
+	}
+	
+	private void execute() {
+		System.out.println("Hello, world.");
 	}
 }
